c69d221162c0204d1ca3dff22205c273b0117d10
noe <noe.reyes92@yahoo.com>
2021-07-06 12:53:31 -0500

    commit tree 4f3174fbec873ebf99503aabcc5cdbfc346f64bc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+      }
+    return thankYouCards
+}
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) { // WHILE loop only needs the condition
+        console.log(startingNumber)
+        startingNumber--
+    }
+}
+
+
+
